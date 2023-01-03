567b7d575c6c82b753ad0cd74a188f623293cd4a
eulbyvan <work.issdq@gmail.com>
2023-01-03 14:56:50 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
  */
 
 const axiosInstance = axios.create({
-    baseURL: "https://localhost:8888",
+    // baseURL: "https://localhost:8888",
     headers: {
         "Content-type": "application/json"
     }
