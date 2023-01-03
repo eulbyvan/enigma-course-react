1ba3422bc8f99541dbe5a04e178b6734523beaaf
eulbyvan <work.issdq@gmail.com>
2023-01-03 14:54:45 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
  */
 
 const axiosInstance = axios.create({
-    // baseURL: "https://jsonplaceholder.typicode.com",
+    baseURL: "https://localhost:8888",
     headers: {
         "Content-type": "application/json"
     }
